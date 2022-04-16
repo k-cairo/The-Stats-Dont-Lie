2ff7730fe15dde8ff5940535e02891073c52703d
CAIRO Kévin <cairo.kevin72@gmail.com>
2022-04-16 23:40:30 +0200

    Add 'get_data' fonction

--- main.py
+++ main.py
@@ -49,9 +49,6 @@ service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 
-# driver.get(f"https://www.thestatsdontlie.com/football/rest-of-the-world/australia/a-league{CARDS}")
-
-
 def get_iframe(link, championship):
     driver.get(link)
     nb_team = TEAMS_IN_CHAMPIONSHIP[championship]
@@ -65,8 +62,51 @@ def get_iframe(link, championship):
             iframe_file.write(f"{content}\n")
 
 
+def get_data():
+    other_teams = []
+    home_teams = []
+    driver.get("https://docs.google.com/spreadsheets/d/1kYzcpxZo90jHOhKz3rCKZtLUtmMcDnv_qKmtRoK_d9I/pubhtml?gid=1157183632&single=true&widget=false&headers=false&chrome=false")
+
+    # Get Home team and Away team
+    teams = driver.find_elements(By.CSS_SELECTOR, "tbody tr td a")
+    for i, team in enumerate(teams):
+        if i % 3 == 0:
+            home_teams.append(team.text)
+        else:
+            other_teams.append(team.text)
+    away_teams = [team for i, team in enumerate(other_teams) if i % 2 == 0]
+
+    # Get Average Stats
+    home_stats = []
+    away_stats = []
+    all_tr = driver.find_elements(By.CSS_SELECTOR, "tbody tr")
+    for tr in all_tr[2:-1]:
+        all_td = tr.find_elements(By.CSS_SELECTOR, "td")
+        home_stat = all_td[4].text
+        away_stat = all_td[9].text
+        home_stats.append(home_stat)
+        away_stats.append(away_stat)
+
+    print(f"Home Teams : {home_teams}")
+    print(f"Home Stats : {home_stats}")
+    print(f"Away Teams : {away_teams}")
+    print(f"Away Stats : {away_stats}")
+
+    return home_stats, home_teams, away_stats, away_teams
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    for key, value in LEAGUES_URLS.items():
-        card_link = value + CARDS
-        get_iframe(link=card_link, championship=key)
+    # for key, value in LEAGUES_URLS.items():
+    #     card_link = value + CARDS
+    #     get_iframe(link=card_link, championship=key)
+    get_data()
+
 
